233f9ccb8ed8af5957d72c527d07112829fdcdf1
Chris McColgan <mccolganchrisj@gmail.com>
2021-06-12 16:22:07 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { Redirect, Route, Switch } from 'react-router-dom';
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+import Layout from './components/layout/Layout';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Layout>
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to='/quotes' />
+        </Route>
+        <Route exact path='/quotes'>
+          <AllQuotes />
+        </Route>
+        <Route exact path='/quotes/add'>
+          <NewQuote />
+        </Route>
+        <Route path='/quotes/:quoteId'>
+          <QuoteDetail />
+        </Route>
+        <Route path='*'>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
